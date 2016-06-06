ac2b1d379170b17eb6b2b6d2151adbc6a896f562
valmsmith39a <valmsmith39a@gmail.com>
2016-06-05 22:39:27 -0700

    adds readme and sets up first part of broker and Paho client with local host/local port for broker/port

--- mainCtrl.js
+++ mainCtrl.js
@@ -5,4 +5,23 @@ app.controller('mainCtrl', function($scope) {
   $scope.test = function(){
     console.log('test succeeds!')
   }
+
+  /*
+    MQTT:
+    MQ Telemtry Transport (MQTT)
+    A network protocol used for publish/subscribe messaging between devices.
+
+    client and broker connected. broker can send client data.
+  */
+
+  var broker = "test.mosquitto.org";
+  var port = 8080;
+
+  var localBroker = location.hostname;
+  var localPort = location.port;
+  var client = new Paho.MQTT.Client(localBroker), Number(localPort), "clientId");
+
+
+
+
 });
